a0851729942d7012567237c2c1204d53b7f1c112
yassineelhoubi <elhoubiyassine@gmail.com>
2021-10-04 15:56:22 +0100

    Register User & Get Token

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@
 // use App/Http/Controllers/ProductController;
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/products/search/{name}' , [ProductController::class , 'search']);
 Route::get('/products' , [ProductController::class , 'index']);
 Route::get('/products/{id}' , [ProductController::class , 'show']);
+
+Route::post('/register' , [AuthController::class , 'register']);
+
+
 //Protected routes
 Route::group(['middleware' =>['auth:sanctum']] ,  function () {
     Route::post('/products' , [ProductController::class , 'store']);
